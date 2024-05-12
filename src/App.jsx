9b2034bae2c23b8656b5e7d219749bfbaa297363
rogernovak <rogernovak377@gmail.com>
2024-05-12 12:20:54 +0530

    create

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signout from './pages/Signout';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 export default function App() {
-  return (
-    <div className='asdasdas text-red-500' >App</div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home />}/>
+    <Route path='/Signin' element={<Signin />}/>
+    <Route path='/Signout' element={<Signout />}/>
+    <Route path='/About' element={<About />}/>
+    <Route path='/Profile' element={<Profile />}/>
+  </Routes>
+  </BrowserRouter>;
 }
